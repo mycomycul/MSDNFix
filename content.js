53f5f1a3ee53748b67d9b5c79025ca9418179a37
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-12-09 11:06:04 -0800

    Adjust resizing to take advantage of prebuilt resizing functions

--- content.js
+++ content.js
@@ -1,37 +1,31 @@
-
-//Add Code to reset percentages onwindow resize
-
-// Create element to inject in page
+// Create Divider Element between the Sidebar and the primary column on page load
 var dividerColumn = document.createElement('div');
 dividerColumn.setAttribute("style", "-webkit-box-flex: 0;-ms-flex: none;flex: none;width: .5%;background-color: white;")
 dividerColumn.id = "dividerColumn";
-dividerColumn.addEventListener("click", function () { shrinkNavColumn(); });
+dividerColumn.addEventListener("click", function () { ResizeColumns(); });
 //Get element to insert after
 var ref = document.getElementById('left-container');
-insertAfter(dividerColumn, ref);
+InsertAfter(dividerColumn, ref);
 
-// var x = document.getElementById("left-container'").previousSibling
-
-function insertAfter(el, referenceNode) {
-    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
-}
-
-function shrinkNavColumn() {
+//Method checks for previous resize and shrink or expands the columns accordingly
+function ResizeColumns() {
     var leftContainer = document.getElementById('left-container');
-    var sidebar = document.getElementById('sidebar')
+    var sidebar = document.getElementById('sidebar');
+    var primaryColumn = leftContainer.previousSibling.previousSibling;
+    //If the sidebar hasn't been resized yet, reduce its width and increase the width of the primary column
     if (leftContainer.style.width != "15%") {
         leftContainer.style.width = "15%";
-        sidebar.style.width = "11%";
+        primaryColumn.style.width = "84.5%";
+        //Use the built in functionality for adjusting the width of the sidebar contents
+        window.dispatchEvent(new Event('resize'));
     }
-    else{
-        
+    //Reset column sizes to their default
+    else {
         leftContainer.style = "";
-        //readjust sidebar by attaching to native width setting function
-        //= window.innerWidth < 1088 ? 
-
-                // (window.innerWidth < 768 ? "0%":"33%") : "25%";
+        primaryColumn.style.width = "";
+        window.dispatchEvent(new Event('resize'));
     }
-    // document.getElementById('main-column').style.width = "89.5%";
-
-
+}
+function InsertAfter(el, referenceNode) {
+    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
 }
