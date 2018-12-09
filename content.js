18942ce51e39b92ee07e7d36957466925d7edb0b
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-12-08 22:55:14 -0800

    Add Functions including adjust width on click

--- content.js
+++ content.js
@@ -1,14 +1,37 @@
 
+//Add Code to reset percentages onwindow resize
+
+// Create element to inject in page
+var dividerColumn = document.createElement('div');
+dividerColumn.setAttribute("style", "-webkit-box-flex: 0;-ms-flex: none;flex: none;width: .5%;background-color: white;")
+dividerColumn.id = "dividerColumn";
+dividerColumn.addEventListener("click", function () { shrinkNavColumn(); });
+//Get element to insert after
+var ref = document.getElementById('left-container');
+insertAfter(dividerColumn, ref);
+
+// var x = document.getElementById("left-container'").previousSibling
+
 function insertAfter(el, referenceNode) {
     referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
 }
 
-// Create element to inject in page
-var newEl = document.createElement('div');
-newEl.setAttribute("style","-webkit-box-flex: 0;-ms-flex: none;flex: none;width: .5%;background-color: white;")
-//Get element to insert after
-var ref = document.querySelector('#left-container');
+function shrinkNavColumn() {
+    var leftContainer = document.getElementById('left-container');
+    var sidebar = document.getElementById('sidebar')
+    if (leftContainer.style.width != "15%") {
+        leftContainer.style.width = "15%";
+        sidebar.style.width = "11%";
+    }
+    else{
+        
+        leftContainer.style = "";
+        //readjust sidebar by attaching to native width setting function
+        //= window.innerWidth < 1088 ? 
 
-insertAfter(newEl, ref);
+                // (window.innerWidth < 768 ? "0%":"33%") : "25%";
+    }
+    // document.getElementById('main-column').style.width = "89.5%";
 
-var x = document.getElementById("left-container'").previousSibling
+
+}
